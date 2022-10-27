b602685c77ae34eb7d6b11548433d06580ef53e3
harun rai <rai.harun64@gmail.com>
2022-10-27 15:57:11 +0545

    added images in the home page

--- home/pages/views.py
+++ home/pages/views.py
@@ -10,25 +10,21 @@ def home(request):
       {
          "service_name":"Password Generator",
          "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-         "image_url":"",
+         "image_url":"images/password-generator.jpg",
          "service_details_urls":"passwordGenerator"
       },
       {
          "service_name":"Email Checker",
          "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-         "image_url":"",
+         "image_url":"images/email_checker.jpg",
          "service_details_urls":"emailchecker-page-home"
       },
       {
          "service_name":"Documents Converter",
          "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-         "image_url":"",
+         "image_url":"images/document-convertor.jpg",
          "service_details_urls":"home"
-      }],
-         
-      "name":"harun"
-      
+      }]    
       }
       
    return render(request, 'dthhome.html', context=service_dict)
-   # return JsonResponse({"Name":"harun"})
